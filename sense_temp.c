69fcb39c3bf2b34e4a67d7ffeab49bae9df44d25
Lorenzo Baroni <lorenzob18@hotmail.com>
2025-04-15 23:43:57 -0300

    feat: Inicialização de todas as portas

--- sense_temp.c
+++ sense_temp.c
@@ -2,6 +2,7 @@
 #include "pico/stdlib.h"
 #include "lib/ssd1306.h"
 #include "hardware/adc.h"
+#include "lib/ws2812.pio.h"
 
 // Configuração dos pinos do biossensor e botões
 #define SENSE 26 
@@ -24,9 +25,60 @@
 #define MATRIX_PIN 7
 #define NUM_LEDS 25
 
+// Variáveis Globais
+ssd1306_t ssd;
+PIO pio = pio0;
+uint offset = 0;
+uint sm = 0;
+uint border_size = 2;
+volatile uint32_t ultimo_tempo_joy = 0;
+volatile uint32_t ultimo_tempo_A = 0;
+volatile uint32_t ultimo_tempo_B = 0;
+const uint32_t debounce = 200;
+
+// Inicializa a Matriz WS2812
+void init_matrix() {
+    offset = pio_add_program(pio, &ws2812_program);
+    sm = pio_claim_unused_sm(pio, true);
+    ws2812_program_init(pio, sm, offset, MATRIX_PIN, 800000, false);
+}
+
 int main()
 {
     stdio_init_all();
+
+    i2c_init(I2C_PORT, 400 * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, DISPLAY_ADDR, I2C_PORT);
+    ssd1306_config(&ssd);
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+
+    adc_init();  
+    adc_gpio_init(SENSE); 
+    adc_select_input(0);
+
+    gpio_init(BUZZER);
+    gpio_set_dir(BUZZER, GPIO_OUT);
+    gpio_put(BUZZER, 0);
+
+    gpio_init(BOTAO_A);
+    gpio_set_dir(BOTAO_A, GPIO_IN);
+    gpio_pull_up(BOTAO_A);
+
+    gpio_init(BOTAO_B);
+    gpio_set_dir(BOTAO_B, GPIO_IN);
+    gpio_pull_up(BOTAO_B);
+
+    gpio_init(JOYSTICK_PB);
+    gpio_set_dir(JOYSTICK_PB, GPIO_IN);
+    gpio_pull_up(JOYSTICK_PB);
+
+    init_matrix();
 
     while (true) {
         sleep_ms(5);

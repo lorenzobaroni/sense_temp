495e62fccdb5eb45bc927c30df97a8a0ff6a7f4e
Lorenzo Baroni <lorenzob18@hotmail.com>
2025-04-15 22:58:20 -0300

    Configurações iniciais

--- sense_temp.c
+++ sense_temp.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "lib/ssd1306.h"
+#include "hardware/adc.h"
 
+// Configuração dos pinos do biossensor e botões
+#define SENSE 26 
+#define JOYSTICK_PB 22
+       
+#define LED_RED 13
+#define LED_GREEN 11
+#define LED_BLUE 12
+#define BUZZER 21
+#define BOTAO_A 5
+#define BOTAO_B 6
 
+// Configuração do I2C para o display OLED
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define DISPLAY_ADDR 0x3C 
+
+// Configuração da matriz de LEDs WS2812
+#define MATRIX_PIN 7
+#define NUM_LEDS 25
 
 int main()
 {
     stdio_init_all();
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        sleep_ms(5);
     }
 }
